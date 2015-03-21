e52c2f8889f760fbe55adaf1631355c3613e1f73
jbalboni <jbalboni@gmail.com>
2015-03-21 00:14:26 -0400

    Remove demo changes

--- demo/grunt/app/index.js
+++ demo/grunt/app/index.js
@@ -3,11 +3,7 @@
 
 	angular.module('AppModule', [])
 		.controller('appCtrl', function(){
-			this.variable = 'tst';
-			this.something();
-			var vm2 = this;
-			var test = this.something;
-			var test = this.something();
+			var variable;
 
 			if(variable === undefined){
 
